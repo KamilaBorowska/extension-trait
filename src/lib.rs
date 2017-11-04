4fd96e233e4d7d2f04c63b1d00d9ea66e7fa2f95
Konrad Borowski <konrad.borowski@kontomatik.com>
2017-11-04 12:10:46 +0100

    Allow public extension traits

--- src/lib.rs
+++ src/lib.rs
@@ -23,4 +23,20 @@ macro_rules! extension_trait {
             fn $fn_name $args $(-> $out_type)* $code
         )* }
     };
+
+    (
+        $(#[$attr:meta])*
+        pub $trait_name:ident for $impl_ty:ty { $(
+            fn $fn_name:ident $args:tt $(-> $out_type:ty)* $code:block
+        )* }
+    ) => {
+        $(#[$attr])*
+        pub trait $trait_name { $(
+            fn $fn_name $args $(-> $out_type)*;
+        )* }
+
+        impl $trait_name for $impl_ty { $(
+            fn $fn_name $args $(-> $out_type)* $code
+        )* }
+    };
 }
