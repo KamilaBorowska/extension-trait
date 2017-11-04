4e8405e866dc0c914a76569b9b548c34d9b7896d
Konrad Borowski <xfix@protonmail.com>
2017-11-04 22:11:23 +0100

    Parse recursive generic brackets

--- src/lib.rs
+++ src/lib.rs
@@ -49,15 +49,15 @@ macro_rules! extension_trait {
     };
 
     (@finish_parsing $($rest:tt)*) => {
-        compile_error!("Incorrect macro syntax");
+        compile_error!(concat!("Incorrect macro syntax, debug info:\n", stringify!($($rest)*)));
     };
 
     (@normalize_block $parsed:tt $block_parsed:tt $pub_token:tt >> $($rest:tt)*) => {
-        extension_trait!(@normalize_block $parsed $block_parsed $pub_token  > > $($rest)*);
+        extension_trait!(@normalize_block $parsed $block_parsed $pub_token > > $($rest)*);
     };
 
     (@normalize_block $parsed:tt $block_parsed:tt $pub_token:tt : $($rest:tt)*) => {
-        extension_trait!(@parse_type_till_end [@normalize_block $parsed] $block_parsed $pub_token [] $($rest)*);
+        extension_trait!(@parse_type_till_end [@normalize_block $parsed] $block_parsed [] $pub_token [] $($rest)*);
     };
 
     (@normalize_block {$($parsed:tt)*} $block_parsed:tt $pub_token:tt) => {
@@ -81,15 +81,31 @@ macro_rules! extension_trait {
     };
 
     (@normalize_expression $parsed:tt $pub_token:tt : $($rest:tt)*) => {
-        extension_trait!(@parse_type_till_end [@normalize_expression] $parsed $pub_token [] $($rest)*);
+        extension_trait!(@parse_type_till_end [@normalize_expression] $parsed [] $pub_token [] $($rest)*);
     };
 
-    (@parse_type_till_end [$($return_trait:tt)*] {$($parsed:tt)*} $pub_token:tt $type_name:tt , $($rest:tt)*) => {
+    (@parse_type_till_end $return_trait:tt $parsed:tt [$($left_brackets:tt)*] $pub_token:tt [$($type_name:tt)*] < $($rest:tt)*) => {
+        extension_trait!(@parse_type_till_end $return_trait $parsed [$($left_brackets)* <] $pub_token [$($type_name)* <] $($rest)*);
+    };
+
+    (@parse_type_till_end $return_trait:tt $parsed:tt $left_brackets:tt $pub_token:tt $type_name:tt >> $($rest:tt)*) => {
+        extension_trait!($return_trait $parsed $left_brackets $pub_token $type_name > > $($rest)*);
+    };
+
+    (@parse_type_till_end [$($return_trait:tt)*] {$($parsed:tt)*} [] $pub_token:tt $type_name:tt > $($rest:tt)*) => {
+        extension_trait!($($return_trait)* {$($parsed)*: $type_name>} $pub_token $($rest)*);
+    };
+
+    (@parse_type_till_end $return_trait:tt $parsed:tt [< $($left_brackets:tt)*] $pub_token:tt [$($type_name:tt)*] > $($rest:tt)*) => {
+        extension_trait!(@parse_type_till_end $return_trait $parsed [$($left_brackets)*] $pub_token [$($type_name)* >] $($rest)*);
+    };
+
+    (@parse_type_till_end [$($return_trait:tt)*] {$($parsed:tt)*} $left_brackets:tt $pub_token:tt $type_name:tt , $($rest:tt)*) => {
         extension_trait!($($return_trait)* {$($parsed)* : $type_name,} $pub_token $($rest)*);
     };
 
-    (@parse_type_till_end $return_trait:tt $parsed:tt $pub_token:tt [$($type_name:tt)*] $token:tt $($rest:tt)*) => {
-        extension_trait!(@parse_type_till_end $return_trait $parsed $pub_token [$($type_name)* $token] $($rest)*);
+    (@parse_type_till_end $return_trait:tt $parsed:tt $left_brackets:tt $pub_token:tt [$($type_name:tt)*] $token:tt $($rest:tt)*) => {
+        extension_trait!(@parse_type_till_end $return_trait $parsed $left_brackets $pub_token [$($type_name)* $token] $($rest)*);
     };
 
     (@normalize_expression {$($parsed:tt)*} $pub_token:tt $parsed_token:tt $($rest:tt)*) => {
@@ -97,7 +113,7 @@ macro_rules! extension_trait {
     };
 
     (@ $($rest:tt)*) => {
-        compile_error!("Parsing has failed before generating final code, this is likely a bug");
+        compile_error!(concat!("Parsing has failed before generating final code, this is likely a bug, debug info:\n", stringify!(@ $($rest)*)));
     };
 
     ($($token:tt)*) => {
